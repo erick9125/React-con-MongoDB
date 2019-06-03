149b3edc8968a1543810f5ae911e397c963e7970
Erick Rodrigo Morales Severino <Erick.morales@correointerno.cl>
2019-06-03 18:32:34 -0400

    se modifica archivo de rutas para generar pruebas

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,10 +3,15 @@ const router = express.Router();
 
 const Task = require('../models/task');
 
-router.get('/', (req, res) => {
-    res.json({
-        status: 'API FUNCIONA'
-    });
+router.get('/', async(req, res) => {
+    const tasks =  await Task.find();
+   console.log(tasks);
+   res.json(tasks);
 });
+
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    res.json('recibido');
+})
 
 module.exports = router;
