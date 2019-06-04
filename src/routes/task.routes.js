cad139f6f99838d21cd3be4f0d9855189814c4a8
Erick Rodrigo Morales Severino <Erick.morales@correointerno.cl>
2019-06-04 10:10:41 -0400

    se termina codigo del servidor con los metodos del CRUD

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,15 +3,40 @@ const router = express.Router();
 
 const Task = require('../models/task');
 
+//metodo para obtener todas las tareas
 router.get('/', async(req, res) => {
     const tasks =  await Task.find();
    console.log(tasks);
    res.json(tasks);
 });
 
+//metodo para obtener solo una tarea
+router.get('/:id', async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    res.json(task);
+});
+
+//metodo para enviar o guardar los datos
 router.post('/', async (req, res) => {
-    console.log(req.body);
-    res.json('recibido');
+    const { titulo , descripcion} =req.body;
+    const task = new Task({titulo, descripcion});
+    await task.save();
+    res.json({status: 'Tarea Guardada'});
+});
+
+//metodo para actualizar
+router.put('/:id', async (req, res) => {
+    const { titulo, descripcion} = req.body;
+    const newTask = { titulo,descripcion};
+    await Task.findByIdAndUpdate(req.params.id, newTask)
+    
+    res.json({status:'Tarea Actualizada'});
 })
+
+//metodo para eliminar datos
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({status: 'Tarea eliminada'});
+});
 
 module.exports = router;
